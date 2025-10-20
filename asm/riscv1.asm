3fbfa3ed5a414055cb4bd1542b25c9e68dde604b
Override <122590935+TuringProblem@users.noreply.github.com>
2025-10-20 14:42:09 -0400

    feat: alright, we started to understand some things I'm going to make the notes for the things that I learn

--- asm/riscv1.asm
+++ asm/riscv1.asm
@@ -1,12 +1,14 @@
 .data
 
 promptOne: .string "Please enter a value to add: "
-promptTwo: .string "Please add another value to add: "
+promptTwo: .string "\nPlease add another value to add: "
 promptThree: .string "The result is: "
+clearConsole: .string "\1bx[H\1bx[2J"
 
 
 .text
 main:
+
 
 
 
@@ -15,9 +17,14 @@ la, a0, promptOne # la here is loading a0 (x10 address to the stack) and using p
 ecall
 
 
+
 #This is still reading the a7 address, and it's second parameter Zero, I'm not sure about yet, but the 5 is the system call to read the interger that is being passed
 addi a7, zero, 5
 ecall
+
+addi a7, zero, 1
+ecall
+
 mv s1, a0
 
 # Now we grabbing the second value but here is the thing, I want to be able to print out the value before actually storing the value inside of a register 
@@ -25,7 +32,11 @@ addi a7, zero 4 # TODO: Figure out how tf to check this in the register - I have
 la, a0, promptTwo
 ecall
 
+
 addi a7, zero, 5
+ecall
+
+addi a7, zero, 1
 ecall
 mv s2, a0
 
